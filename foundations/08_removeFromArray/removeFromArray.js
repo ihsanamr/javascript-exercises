9f68455b1dce97c44ee9d175500be3f58a20c0de
Ihsan Amri <ihsanamrimuyassar@gmail.com>
2026-03-23 21:53:22 +0700

    Finished foundations exercise (06-11)

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+// First Solution (with filter)
+// const removeFromArray = function (array, ...args) {
+//   return (cleanedArray = array.filter((val) => !args.includes(val)));
+// };
+
+// Second Solution (manual)
+const removeFromArray = function (array, ...args) {
+  let cleanedArray = [];
+
+  array.forEach((element) => {
+    if (!args.includes(element)) {
+      cleanedArray.push(element);
+    }
+  });
+
+  return cleanedArray;
 };
 
 // Do not edit below this line
