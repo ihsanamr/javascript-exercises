316524873bf4bae1b5a39acf8ed33004cb5ecc28
Ihsan Amri <ihsanamrimuyassar@gmail.com>
2026-04-07 22:32:09 +0700

    Finished exrecise 12 and 13

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length === 0) return 0;
+  const finalSum = array.reduce((acc, num) => {
+    return (acc += num);
+  });
+  return finalSum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  if (array.length === 0) return 0;
+  const finalMultiply = array.reduce((acc, num) => {
+    return (acc *= num);
+  });
+  return finalMultiply;
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let validChars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let lowerString = string.toLowerCase();
+  let splittedString = lowerString.split("");
+  let cleanedString = "";
+  let reversedString = "";
 
+  for (let i = splittedString.length - 1; i >= 0; i--) {
+    let char = splittedString[i].toLowerCase();
+    if (validChars.includes(char)) {
+      cleanedString += char;
+      reversedString = char + reversedString;
+      console.log(cleanedString);
+      console.log(reversedString);
+    }
+  }
+  return cleanedString === reversedString ? true : false;
 };
+
+palindromes("r3ace3car");
 
 // Do not edit below this line
 module.exports = palindromes;
