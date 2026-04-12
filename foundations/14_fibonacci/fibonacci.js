f644dcd35807039dee91822977a8b83c4ef90026
Ihsan Amri <ihsanamrimuyassar@gmail.com>
2026-04-12 18:19:32 +0700

    Finished JS exercise number 14 and 15

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  // // iterative apporach
+  // let fiboNum = [0, 1];
+  // if (n < 0) return "OOPS";
+  // if (n === 0) return fiboNum[0];
+  // if (n === 1) return fiboNum[1];
+  // for (let i = 0; i < n; i++) {
+  //   fiboNum.push(fiboNum[i] + fiboNum[i + 1]);
+  // }
+  // return fiboNum[n];
 
+  // recursive
+  if (n < 0) {
+    return "OOPS";
+  } else if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 };
 
 // Do not edit below this line
